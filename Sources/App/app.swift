5b5b9a4a6527562fe98f61ac7b4fd1a380c52f44
Ivan Sapozhnik <sapozhnik.ivan@gmail.com>
2019-12-10 19:30:44 +0100

    TelegramBotSDK added

--- Sources/App/app.swift
+++ Sources/App/app.swift
@@ -1,4 +1,11 @@
 import Vapor
+import Core
+import TelegramBotSDK
+
+let dirConfig = DirectoryConfig.detect()
+let token = readToken(from: dirConfig.workDir+"/"+"HELLO_BOT_TOKEN")
+let bot = TelegramBot(token: token)
+let router = Router(bot: bot)
 
 /// Creates an instance of `Application`. This is called from `main.swift` in the run target.
 public func app(_ env: Environment) throws -> Application {
@@ -8,5 +15,24 @@ public func app(_ env: Environment) throws -> Application {
     try configure(&config, &env, &services)
     let app = try Application(config: config, environment: env, services: services)
     try boot(app)
+    
+    router[.location] = { context in
+        guard let from = context.message?.from, let location = context.message?.location else { return false }
+        context.respondAsync("Hello, \(from.firstName)! Your location: \(location.latitude), \(location.longitude)")
+        return true
+    }
+    
+    router.unsupportedContentType = { context in
+        guard let from = context.message?.from, let location = context.message?.location else { return false }
+        context.respondAsync("Hello, \(from.firstName)! New location: \(location.latitude), \(location.longitude)")
+        return true
+    }
+    
+    while let update = bot.nextUpdateSync() {
+        try router.process(update: update)
+    }
+
+    fatalError("Server stopped due to error: \(bot.lastError)")
+    
     return app
 }
--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -1,3 +1,15 @@
 import App
 
+//let dirConfig = DirectoryConfig.detect()
+//
+//let token = readToken(from: dirConfig.workDir+"/"+"HELLO_BOT_TOKEN")
+//let bot = TelegramBot(token: token)
+
+//while let update = bot.nextUpdateSync() {
+//    if let message = update.message, let from = message.from, let text = message.text {
+//        bot.sendMessageAsync(chatId: from.id,
+//                             text: "Hi \(from.firstName)! You said: \(text).\n")
+//    }
+//}
+
 try app(.detect()).run()
